773025213556a48eb782ae88a353724c42338208
jhs99126 <jhs99126@naver.com>
2024-07-06 18:09:57 +0900

    Mapping Config Change

--- src/main/java/cj/cloudwave/member/controller/MemberController.java
+++ src/main/java/cj/cloudwave/member/controller/MemberController.java
@@ -10,12 +10,10 @@ import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.BindingResult;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
+@RequestMapping("/api")
 @RequiredArgsConstructor
 public class MemberController {
     private final MemberService memberService;
