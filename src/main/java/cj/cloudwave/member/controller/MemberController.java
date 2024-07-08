a422407a7f2fbe81eb6de626d182cd148ae0cf8e
jhs99126 <jhs99126@naver.com>
2024-07-08 16:02:46 +0900

    config change

--- src/main/java/cj/cloudwave/member/controller/MemberController.java
+++ src/main/java/cj/cloudwave/member/controller/MemberController.java
@@ -13,6 +13,7 @@ import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
+@RequestMapping("/api")
 @RequiredArgsConstructor
 public class MemberController {
     private final MemberService memberService;
