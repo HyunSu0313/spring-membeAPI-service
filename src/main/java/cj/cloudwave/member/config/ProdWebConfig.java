f6291ac3cae8b74f1350590fc5f3b3e8b67becd2
jhs99126 <jhs99126@naver.com>
2024-07-11 00:09:48 +0900

    config change

--- src/main/java/cj/cloudwave/member/config/ProdWebConfig.java
+++ src/main/java/cj/cloudwave/member/config/ProdWebConfig.java
@@ -11,7 +11,7 @@ public class ProdWebConfig implements WebMvcConfigurer {
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/api/**")
+        registry.addMapping("/**")
                 .allowedOrigins("*")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
