4513476161a0f2723d3a9e98a1695424d42321ad
jhs99126 <jhs99126@naver.com>
2024-07-11 20:30:57 +0900

    config change

--- src/main/java/cj/cloudwave/member/controller/MemberController.java
+++ src/main/java/cj/cloudwave/member/controller/MemberController.java
@@ -8,10 +8,12 @@ import cj.cloudwave.member.service.MemberService;
 import jakarta.servlet.http.HttpSession;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
 
+@Slf4j
 @RestController
 @RequiredArgsConstructor
 public class MemberController {
